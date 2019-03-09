ecbc5757c6ff57223cacd6468b1933f2e17bad75
raristo <raristo@msn.com>
2019-03-09 11:58:32 -0500

    add  multiselect

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -25,9 +25,17 @@ import HackImg from "../Graphics/ncciHackathonLogo.png";
 
 import HackImg from "../assests/hack.PNG"
 
-class Register extends React.Component {
-  allItems = [{ id: 1, name: 'nodeJs' }, { id: 2, name: 'reactJs' }];
+const skillOptions = [
+  'nodeJs',
+  'React',
+  'Ng',
+  'C#',
+  'Java',
+  'Sql Server',
+  'Oracle',
+];
 
+class Register extends React.Component {
   constructor(props) {
     super(props);
 
