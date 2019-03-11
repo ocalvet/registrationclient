23396c5c16f58880d5501f7658351d37ba6c07a3
lumpypuppy <lumpypuppy@gmail.com>
2019-03-11 11:36:24 -0400

    added front page with graphics

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,48 @@
 import React from "react";
-import { Link } from "react-router-dom"
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+import style from '../src/homeStyle.css';
+import logo from './Graphics/NCCI_LogoWhite_TealTransparent.png';
+import hackathonLogo from './Graphics/hackathonSVG.svg';
+import Faq from './FrequentlyAskedQuestions';
+import About from './About';
+import Menu from './MenuComponent'
 
 class Home extends React.Component {
 
     render() {
 		return (
             <div>
-                <Typography variant="h1" color="primary">Home</Typography>
-                <Link to="/Register" style={{ textDecorationLine: 'none' }}><Button variant="outlined" color="secondary">Register</Button></Link>
-            </div>
+                <div class="menu-body"><Menu /></div>
+                <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'></link>
+                <link href='https://fonts.googleapis.com/css?family=Oswald:300' rel='stylesheet' type='text/css'></link>
+                <div class="stripe--1">
+                    <div class="text-box--1">
+                    <h1><img src={logo} alt="NCCI"></img>NCCI Hackathon.</h1>
+                    <p>Presented by the Innovation Group.</p>
+                    <p><img src={hackathonLogo} alt="logo" class="hackathon_Logo"></img></p>
+                    </div>
+                </div>
+                <div class="text-box--4">
+                <a href="#" class="action-button shadow animate red"><Link to="/Register">Register</Link></a>
+                </div>
+                <div class="text-box--2">
+                    <h1>A place to get your hack-on</h1>
+                    <p>Pure Awesome.</p>
+                </div>
+                
+                <div class="text-box--3">
+                <About />
+                <Faq />
+                </div> 
+
+                <div>
+                <footer>
+                    <p>Copyright © 2015</p>
+                </footer>
+                </div>
+                
+                     
+            </div>                   
         )
     }
 }
---
+++ src/MenuComponent.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+class Menu extends React.Component {
+    render(){
+        return (
+            <div >
+                <a href="#" class="btn">Home</a>
+                <a href="#" class="btn">Register</a>
+                <a href="#" class="btn">FAQ</a>
+                <a href="#" class="btn">Contact</a>
+
+            </div>
+        )
+    }
+}
+
+export default Menu;
