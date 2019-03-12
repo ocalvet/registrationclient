c2a6a91f9bea273d6b28d27260faeeb7f614b141
aprja <rene_aristorenas@ncci.com>
2019-03-12 10:03:58 -0400

    incorperate designs into registration

---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class About extends React.Component {
+    render(){
+        return (
+            <div>
+                    <h1>About</h1>
+                    <p>
+                    The NCCI Hackathon is the companies first hackathon. We are seeking to bring together the best and brightest hackers, developers, creatives, artists, and novices from across the company for 24 hours of coding, designing, and jam-packed fun.</p>
+                    <br></br>
+                    <br></br>
+                    <p>We’ll provide you with all the resources, mentorship, and food to do your thing; all you need to bring is your creativity and ideas. The Hackathon is also a gathering of some of the top developers. Whether you’re looking for creative, networking, or corporate opportunities, come ready to flex your networking game! Our hackathon is open to all levels: no matter if you’re a beginner or veteran, it’s an event not to miss!
+                    </p>
+
+            </div>
+        )
+    }
+}
+
+export default About;
---
+++ src/FrequentlyAskedQuestions.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+class Faq extends React.Component {
+    render(){
+        return (
+            <div> 
+                    <h1>Frequently Asked Questions</h1>
+                    <h2>General</h2>
+                    <b>Do you really "hack" at a hackathon?</b>
+                    <p><br></br><br></br>No, we are not doing or encouraging illegal activities here.</p>
+
+                    <b>So what is a hackathon then?</b>
+                    <p><br></br><br></br>A hackathon is a live-event where participants, “hackers”, spend the course of 24 hours creating, developing, designing, and pitching a software or hardware project they build from scratch. We provide all the resources and mentorship you need in order to make this happen!</p>
+
+                    <b>How much does it cost to attend?</b>
+                    <p><br></br><br></br>Absolutely nothing! It is a completely free event run by us. All resources, and accommodations for the entire event will be provided free of charge. Somw snacks will be provided too!</p>
+
+                    <b>How do I get to Hack</b>
+                    <p><br></br><br></br>This event will be hosted here on premise.</p>
+
+                    <b>Will you be providing travel reimbursements?</b>
+                    <p><br></br><br></br>Unfortunately, we will not be covering travel reimbursements at this time.</p>
+
+                    <b>What kind of workshops and activities will be hosted?</b>
+                    <p><br></br><br></br>We’ll be hosting a variety of workshops from beginner subjects, such as introduction to coding, to advanced courses, such as security penetration testing. No matter your interest, we guarantee we’ll have something for you. In addition, we have a ton of fun activities planned for you such as e-sports tournaments, nugget-eating contests, and more! The NCCI Hackathon is much more than just a hackathon – we make sure that it’s an event you’ll thoroughly enjoy.</p>
+
+                    <b>What should I bring?</b>
+                    <p><br></br><br></br>Make sure to bring your laptop (or desktop if you’ve got gains) and your imagination. You can also bring a pillow and blanket if you want to get comfy. Everything else will be provided for you!</p>
+            </div>
+        )
+    }
+}
+
+export default Faq;
